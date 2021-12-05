5a2162137165aa80d320824534e6cc0b389af22e
zhaomin <min.zhao@newsbreak.com>
2021-12-05 15:43:04 +0800

    first commit

---
+++ metrics.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from sklearn import metrics
+
+
+def cal_group_auc(datas):
+    uid_auc_list = []
+    for uid, data_list in datas:
+        labels = []
+        scores = []
+        for label, score in datas:
+            labels.append(label)
+            scores.append(score)
+        uid_auc = metrics.roc_auc_score(labels, scores)
+        uid_auc_list.append(uid_auc)
+    return sum(uid_auc_list) / len(uid_auc_list) if uid_auc_list != 0 else -1
+
+
+def cal_auc(results):
+    """返回基于样本粒度的auc以及基于uid的groud auc."""
+    y_label = []
+    y_predict = []
+
+    uid_datas = {}
+    for uid, label, predict in results:
+        y_label.append(label)
+        y_predict.append(predict)
+
+        if uid not in uid_datas:
+            uid_datas[uid] = []
+        uid_datas[uid].append((y_label, y_predict))
+
+    sample_auc = metrics.roc_auc_score(y_label, y_predict)
+    uid_auc = cal_group_auc(uid_datas)
+    return sample_auc, uid_auc
---
+++ train.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import argparse
+import os
+import logging
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
+
+from utils import data_preprocess
+from net import DeepFM
+import data_process
+
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s',
+    level=logging.INFO)
+
+
+def run_trainer(args):
+    if torch.cuda.is_available():
+        device = 'gpu'
+    else:
+        device = 'cpu'
+    user_file, movie_file, rating_file = args.users_data, args.movies_data, args.ratings_data
+    data = data_preprocess(user_file, movie_file, rating_file)
+    train_data, test_data = train_test_split(data, test_size=0.2, train_size=0.8, random_state=0)
+    training_set = data_process.JoinedDataset(
+        train_data, args.single_feature_col, args.multi_feature_col, args.label_col)
+    testing_set = data_process.JoinedDataset(
+        test_data, args.single_feature_col, args.multi_feature_col, args.label_col)
+    training_params = {
+        'batch_size': args.batch,
+        'num_workers': 6,
+        'drop_last': True,
+        'shuffle': True,
+    }
+    testing_params = {
+        'batch_size': 512,
+        'num_workers': 6,
+        'pin_memory': True,
+        'drop_last': False,
+    }
+    training_generator = DataLoader(training_set, **training_params)
+    testing_generator = DataLoader(testing_set, **testing_params)
+    net = DeepFM(device)
+    if device == 'gpu':
+        net.cuda()
+    # Setup optimizer
+    optimizer = optim.Adam(net.parameters(), lr=args.lr)
+    # 如果是接着训练的话，需要把上次的训练结果导入，然后继续进行训练
+    if args.base_optimizer is not None:
+        ckpt = torch.load(args.base_optimizer)
+        optimizer.load_state_dict(ckpt['optimizer_state_dict'])
+        for g in optimizer.param_groups:
+            g['lr'] = args.lr
+        del ckpt
+    criterion = nn.BCELoss()
+
+    train_params = {
+        'net': net,
+        'optimizer': optimizer,
+        'criterion': criterion,
+        'epochs': args.epoch,
+        'training_generator': training_generator,
+        'testing_generator': testing_generator,
+        'model_name': os.path.join(args.output, 'model.bin'),
+        'optimizer_name': os.path.join(args.output, 'optimizer.pkl'),
+        'device': device,
+    }
+    train(**train_params)
+
+
+def test(net, testing_generator, device='cpu'):
+    net.eval()
+    results = []
+    with torch.no_grad():
+        for i, batch in enumerate(testing_generator):
+            if device == 'gpu':
+                for k in batch:
+                    if not isinstance(batch[k], list):
+                        batch[k] = batch[k].to('cuda', non_blocking=True)
+
+            outs = net.predict(batch)
+            for uid, label, predict in zip(
+                    batch['uid'].tolist(), batch['label'].tolist(), outs.tolist()):
+                results.append((uid, label, predict))
+    net.train()
+    return results
+
+
+def train(net,
+          optimizer,
+          criterion,
+          epochs,
+          training_generator,
+          testing_generator,
+          model_name,
+          optimizer_name,
+          device='cpu'):
+    for epoch in range(epochs):
+        for i, batch in enumerate(training_generator):
+            if i != 0 and i % 5000 == 0:
+                total_auc, uid_auc = test(net, testing_generator, device)
+                logging.info(f"test total auc is: {total_auc}, test uid auc is: {uid_auc}")
+            if device == 'gpu':
+                for k in batch:
+                    if not isinstance(batch[k], list):
+                        batch[k] = batch[k].to('cuda', non_blocking=True)
+            optimizer.zero_grad()
+            output = net(batch)
+            labels = batch['label'].unsqueeze(-1).float()
+            if device == 'gpu':
+                labels = labels.cuda()
+            loss = criterion(output, labels)
+            loss.backward()
+            logging.info(f'Epoch {epoch},{i} Loss {loss.item()}')
+        MODEL_SAVE_PATH = f'{model_name}.{epoch}'
+        OPTIMIZER_SAVE_PATH = f'{optimizer_name}.{epoch}'
+        torch.save({
+            'model_state_dict': net.state_dict(),
+        }, MODEL_SAVE_PATH)
+        torch.save({
+            'optimizer_state_dict': optimizer.state_dict(),
+        }, OPTIMIZER_SAVE_PATH)
+        total_auc, uid_auc = test(net, testing_generator, device)
+        logging.info(f"test total auc is: {total_auc}, test uid auc is: {uid_auc}")
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--users_data", default='movielens/users.dat')
+    arg_parser.add_argument("--movies_data", default='movielens/movies.dat')
+    arg_parser.add_argument("--ratings_data", default='movielens/ratings.dat')
+    arg_parser.add_argument("--single_feature_col", default=["uid", "movieid", "gender", "age", "occ", "zip_code"])
+    arg_parser.add_argument("--multi_feature_col", default=["genres"])
+    arg_parser.add_argument("--label_col", default="label")
+    arg_parser.add_argument('--lr', type=float, help='learning rate', default=0.005)
+    arg_parser.add_argument('--batch', type=int, help='batch_size', default=512)
+    arg_parser.add_argument('--epoch', type=int, default=4)
+    arg_parser.add_argument('--output', help='output model path', default='./model/')
+    arg_parser.add_argument('--base_model', nargs='?', default=None, help='use for fine tune')
+    arg_parser.add_argument('--base_optimizer', nargs='?', default=None, help='use for fine tune')
+    args = arg_parser.parse_args()
+    run_trainer(args)
+
+
+if __name__ == '__main__':
+    main()
